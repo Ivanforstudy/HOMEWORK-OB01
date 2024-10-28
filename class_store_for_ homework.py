402eb170f4f24ec349a76b9c4f32e22fc2d24058
IvanTusanin <ivantusanin@gmail.com>
2024-10-28 16:39:26 +0300

    ADD FOR HOMEWORK OB01 2.2

---
+++ class_store_for_ homework.py
@@ -0,0 +1,50 @@
+class Store:
+    def __init__(self, name, address):
+        self.name = name
+        self.address = address
+        self.items = {}
+
+    def add_item(self, item_name, price):
+        self.items[item_name] = price
+        print(f"Курс '{item_name}' добавлен в университет'{self.name}' по цене {price}.")
+
+    def remove_item(self, item_name):
+        if item_name in self.items:
+            del self.items[item_name]
+            print(f"Курс '{item_name}' удален из университета '{self.name}'.")
+        else:
+            print(f"Курс '{item_name}' не найден в университете '{self.name}'.")
+
+    def get_item_price(self, item_name):
+        return self.items.get(item_name, None)
+
+    def update_item_price(self, item_name, new_price):
+        if item_name in self.items:
+            self.items[item_name] = new_price
+            print(f"Стоимость курса '{item_name}' обновлена до {new_price} в университете '{self.name}'.")
+        else:
+            print(f"Курс '{item_name}' не найден в университете '{self.name}'.")
+
+
+# Создание объектов класса Store
+store1 = Store("ZEROCODER", "https://zerocoder.ru/")
+
+store2 = Store("ZEROCODER", "https://zerocoder.ru/")
+store3 = Store("ZEROCODER", "https://zerocoder.ru/")
+# Добавление товаров в магазины
+store1.add_item("Курс супердизайнеров", 1000000)
+store1.add_item("Курс супертестировщиков", 1000000)
+
+store2.add_item("Курс суперверсточников", 1000000)
+store2.add_item("Курс суперкодеров", 1000000)
+
+
+store3.add_item("Курс киберджедаев", 1000000)
+store3.add_item("Курс кибергениев", 1000000)
+
+print("\nТестирование курсов униерситета 'ZEROCODER':")
+store2.add_item("ПРОМПТ Инженеров", 1000000)  # Добавление товара
+print(f"Халява: {store2.get_item_price('Халява')}")  # Получение цены товара
+store2.update_item_price("Халява", 0.8)  # Обновление цены товара
+store2.remove_item("Благотворительность")  # Удаление товара
+print(f"Цена Благотворительности после удаления: {store2.get_item_price('халява')}")  # Проверка цены удаленного товара
